13037dabb18e44ccde4c7119fd2595bcdf4c9021
Timothy Sukamtoh <timtoh168@gmail.com>
2023-06-15 14:42:39 -0700

    add recursive tests

--- graph.test.js
+++ graph.test.js
@@ -211,6 +211,78 @@ describe("DFS", function () {
   });
 });
 
+describe("DFS recursive", function () {
+  it("return an array of the nodes searched using DFS recursively", function () {
+    // build complex graph
+    //
+    //                       Q --P -- S
+    //                      /  \ |      \
+    //                     R     X   --  U
+    //                     | \   |  \   /
+    //                     |  \  |    V
+    //                      \    Y    |
+    //                       \     \  /
+    //                         T --- W
+    //
+
+    let graph = new Graph();
+    let S = new Node("S");
+    let P = new Node("P");
+    let U = new Node("U");
+    let X = new Node("X");
+    let Q = new Node("Q");
+    let Y = new Node("Y");
+    let V = new Node("V");
+    let R = new Node("R");
+    let W = new Node("W");
+    let T = new Node("T");
+
+    graph.addVertices([S, P, U, X, Q, Y, V, R, W, T]);
+
+    graph.addEdge(S, P);
+    graph.addEdge(S, U);
+
+    graph.addEdge(P, X);
+    graph.addEdge(U, X);
+
+    graph.addEdge(P, Q);
+    graph.addEdge(U, V);
+
+    graph.addEdge(X, Q);
+    graph.addEdge(X, Y);
+    graph.addEdge(X, V);
+
+    graph.addEdge(Q, R);
+    graph.addEdge(Y, R);
+
+    graph.addEdge(Y, W);
+    graph.addEdge(V, W);
+
+    graph.addEdge(R, T);
+    graph.addEdge(W, T);
+
+    let result = graph.depthFirstSearchRecursive(T);
+
+    expect.assertions(2);
+
+    if (result[1] === "R") {
+      expect(result[2] === "Q" || result[2] === "Y").toBe(true);
+      if (result[2] === "Q") {
+        expect(result[3] === "P" || result[3] === "X").toBe(true);
+      } else {
+        expect(result[3] === "X" || result[3] === "W").toBe(true);
+      }
+    } else if (result[1] === "W") {
+      expect(result[2] === "Y" || result[2] === "V").toBe(true);
+      if (result[2] === "Y") {
+        expect(result[3] === "X" || result[3] === "R").toBe(true);
+      } else {
+        expect(result[3] === "X" || result[3] === "U").toBe(true);
+      }
+    }
+  });
+});
+
 describe("BFS", function () {
   it("should return an array of the nodes searched using BFS", function () {
     // build complex graph
@@ -262,6 +334,70 @@ describe("BFS", function () {
     graph.addEdge(W, T);
 
     let results = graph.breadthFirstSearch(S);
+
+    expect(results.indexOf("P") < results.indexOf("Y")).toBe(true);
+    expect(results.indexOf("P") < results.indexOf("V")).toBe(true);
+    expect(results.indexOf("P") < results.indexOf("Q")).toBe(true);
+    expect(results.indexOf("U") < results.indexOf("Y")).toBe(true);
+    expect(results.indexOf("U") < results.indexOf("V")).toBe(true);
+    expect(results.indexOf("U") < results.indexOf("Q")).toBe(true);
+    expect(results.indexOf("X") < results.indexOf("Y")).toBe(true);
+    expect(results.indexOf("X") < results.indexOf("V")).toBe(true);
+    expect(results.indexOf("X") < results.indexOf("Q")).toBe(true);
+  });
+});
+
+describe("BFS recursive", function () {
+  it("should return an array of the nodes searched using BFS recursively", function () {
+    // build complex graph
+    //
+    //                       Q --P -- S
+    //                      /  \ |      \
+    //                     R     X  ---  U
+    //                     | \   |  \   /
+    //                     |  \  |    V
+    //                      \    Y    |
+    //                       \     \  /
+    //                         T --- W
+    //
+
+    let graph = new Graph();
+    let S = new Node("S");
+    let P = new Node("P");
+    let U = new Node("U");
+    let X = new Node("X");
+    let Q = new Node("Q");
+    let Y = new Node("Y");
+    let V = new Node("V");
+    let R = new Node("R");
+    let W = new Node("W");
+    let T = new Node("T");
+
+    graph.addVertices([S, P, U, X, Q, Y, V, R, W, T]);
+
+    graph.addEdge(S, P);
+    graph.addEdge(S, U);
+
+    graph.addEdge(P, X);
+    graph.addEdge(U, X);
+
+    graph.addEdge(P, Q);
+    graph.addEdge(U, V);
+
+    graph.addEdge(X, Q);
+    graph.addEdge(X, Y);
+    graph.addEdge(X, V);
+
+    graph.addEdge(Q, R);
+    graph.addEdge(Y, R);
+
+    graph.addEdge(Y, W);
+    graph.addEdge(V, W);
+
+    graph.addEdge(R, T);
+    graph.addEdge(W, T);
+
+    let results = graph.breadthFirstSearchRecursive(S);
 
     expect(results.indexOf("P") < results.indexOf("Y")).toBe(true);
     expect(results.indexOf("P") < results.indexOf("V")).toBe(true);
