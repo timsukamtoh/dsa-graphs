13de039a9eddf54c7cb0e1a34b80821d8e404907
Timothy Sukamtoh <timtoh168@gmail.com>
2023-06-15 11:36:38 -0700

    setup graph methods

--- graph.js
+++ graph.js
@@ -1,3 +1,4 @@
+"use strict";
 /** Node class for graph. */
 
 class Node {
@@ -16,23 +17,40 @@ class Graph {
   }
 
   /** add Node instance and add it to nodes property on graph. */
-  addVertex(vertex) { }
+  addVertex(vertex) {
+    this.nodes.add(vertex);
+  }
 
   /** add array of new Node instances and adds to them to nodes property. */
-  addVertices(vertexArray) { }
+  addVertices(vertexArray) {
+    for (const vertex of vertexArray) {
+      this.addVertex(vertex);
+    }
+  }
 
   /** add edge between vertices v1,v2 */
-  addEdge(v1, v2) { }
+  addEdge(v1, v2) {
+    v1.adjacent.add(v2);
+    v2.adjacent.add(v1);
+  }
 
   /** remove edge between vertices v1,v2 */
-  removeEdge(v1, v2) { }
+  removeEdge(v1, v2) {
+    v1.adjacent.delete(v2);
+    v2.adjacent.delete(v1);
+  }
 
   /** remove vertex from graph:
    *
-   * - remove it from nodes property of graph
    * - update any adjacency lists using that vertex
+   * - remove it from nodes property of graph
    */
-  removeVertex(vertex) { }
+  removeVertex(vertex) {
+    for (const neighbor of vertex.adjacent) {
+      this.removeEdge(vertex, neighbor)
+    }
+    this.nodes.delete(vertex);
+  }
 
   /** traverse graph with DFS and returns array of Node values */
   depthFirstSearch(start) { }
@@ -44,4 +62,4 @@ class Graph {
   distanceOfShortestPath(start, end) { }
 }
 
-module.exports = { Graph, Node }
+module.exports = { Graph, Node };
